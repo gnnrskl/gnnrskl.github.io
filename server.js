565c83a9ee2dabe5be9121f5ee540b96b8174900
gnnrskl <34112014+gnnrskl@users.noreply.github.com>
2018-05-31 08:49:17 -0400

    Update server.js

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ io.on('connection', function(socket) {
             width: 10,
             height: 10,
             id: getRandomInt(9999),
-            color: 'black'
+            color: '#'+(Math.random()*0xFFFFFF<<0).toString(16);
         };
     });
     
